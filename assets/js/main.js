ab03544a3a1027def69ee86bbb65759a2b860eef
mahmoudAbdElfattah88 <76801268+mahmoudAbdElfattah88@users.noreply.github.com>
2021-12-19 01:05:34 +0200

    scound commit

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+// toggle menu
+document.querySelector(".toggle").addEventListener("click",function (e){
+    this.classList.toggle("active");
+    document.querySelector("nav").classList.toggle("active");
+    
+})
